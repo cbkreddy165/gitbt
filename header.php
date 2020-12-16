815dcaf5cd3dea1558f65010ea8596d8d93e4f06
cbkreddy165 <68158468+cbkreddy165@users.noreply.github.com>
2020-12-16 21:18:17 +0530

    Update header.php
    test

--- header.php
+++ header.php
@@ -1,1 +1,71 @@
-<!DOCTYPE html><html lang="en">    <head>        <meta charset="utf-8">        <meta http-equiv="X-UA-Compatible" content="IE=edge">        <meta name="viewport" content="width=device-width, initial-scale=1">        <meta name="description" content="">        <meta name="author" content="">        <title>Fosho Technologies - We take care your business</title>        <!-- Bootstrap Core CSS -->        <link href="css/bootstrap.min.css" rel="stylesheet">        <!-- Custom CSS -->        <link href="css/style.css" rel="stylesheet">               <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">		<link rel="stylesheet" href="css/animate.css">		       <!-- Custom Fonts -->        <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->        <!--[if lt IE 9]>            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>        <![endif]-->    </head>    <body>        <nav class="navbar navbar-default-home navbar-default mymenu navbar-fixed-top">            <div class="container nav-80">                <div class="navbar-header">                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">                        <span class="sr-only">Toggle navigation</span>                        <span class="icon-bar"></span>                        <span class="icon-bar"></span>                        <span class="icon-bar"></span>                    </button>                    <div class="navbar-brand">                        <a href="http://localhost/foshotechNew">                             <img src="images/fosho-logo.png"/>                        </a>                    </div>                </div>                <div id="navbar" class="navbar-collapse collapse cust-menu">                    <ul class="nav navbar-nav cust-menu">                        <li ><a href="index.php">Home</a></li>                        <li  class="dropdown "><a href="aboutus.php">About Us</a>                            <!-- <ul class="dropdown-menu" >                                <li ><a href="ourteam.html">Our Team</a></li>                                <li ><a href="testimoanils.html">Testimonials</a></li>                            </ul> -->                        </li>                        <li><a href="services.php">Services</a></li>                        <li><a href="#">Solutions</a></li>                        <li><a href="#">Blog</a></li>                        <li><a href="ourteam.php">Resources</a></li>                        <li ><a href="contactus.php">Contact Us</a></li>                    </ul>                           </div><!--/.nav-collapse -->            </div>        </nav>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Fosho Technologies - We take care your business</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+       
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/animate.css">
+		
+       <!-- Custom Fonts -->
+        <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-default-home navbar-default mymenu navbar-fixed-top">
+            <div class="container nav-80">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="navbar-brand">
+                        <a href="http://localhost/foshotechNew">
+                             <img src="images/fosho-logo.png"/>
+                        </a>
+                    </div>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse cust-menu">
+                    <ul class="nav navbar-nav cust-menu">
+                        <li ><a href="index.php">Home</a></li>
+                        <li  class="dropdown "><a href="aboutus.php">About Us</a>
+                            <!-- <ul class="dropdown-menu" >
+                                <li ><a href="ourteam.html">Our Team</a></li>
+                                <li ><a href="testimoanils.html">Testimonials</a></li>
+                            </ul> -->
+                        </li>
+                        <li><a href="services.php">Services</a></li>
+                        <li><a href="#">Solutions</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="ourteam.php">Resources</a></li>
+                        <li><a href="ourteam.php">About</a></li>
+                        <li ><a href="contactus.php">Contact Us</a></li>
+                    </ul>           
+
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
